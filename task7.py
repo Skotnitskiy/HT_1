3e908265d7943b62f8db91241db4bb0fa5c21d0c
serhSK <sergeyskotnitskiy@gmail.com>
2017-10-21 19:27:47 +0300

    added task7

---
+++ task7.py
@@ -0,0 +1,4 @@
+# Write a script to concatenate all elements in a list into a string and print it
+strings_list = ['a', 'b', 'cd', 'i', 'fj', 'pg', 'bv']
+result = ''
+print(result.join(strings_list))
