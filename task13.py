7a0dd501eeb6ceb10fb3468fbb3e47659a26f984
serhSK <sergeyskotnitskiy@gmail.com>
2017-10-21 20:33:04 +0300

    added task 13

---
+++ task13.py
@@ -0,0 +1,4 @@
+# Write a script to get the maximum and minimum value in a dictionary
+d = {'a': 25, 'b': 20, 'c': 70, 'd': 0, 'e': 8}
+print('Max = ', max(d.values()))
+print('Min = ', min(d.values()))
